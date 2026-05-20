1f25eb099988587692e8c336628e13b7b897ba6d
ToNyRANDRIAMANANTSOA <tonyrandriamanantsoa.to@gmail.com>
2026-05-20 11:40:50 +0300

    (fix) tebokaroa-common-lib-subset: exclude esm-patient-common-lib from being skipped on typescript transpilation, on esm-patient-notes-app and esm-patient-tests-app, rspack.config.js build config

--- packages/esm-patient-notes-app/rspack.config.js
+++ packages/esm-patient-notes-app/rspack.config.js
@@ -1,1 +1,5 @@
-module.exports = require('openmrs/default-rspack-config');
+const config = require('openmrs/default-rspack-config');
+
+config.scriptRuleConfig.exclude = /node_modules\/(?!(@openmrs|@tebokaroa)\/esm-patient-common-lib)/;
+
+module.exports = config;
