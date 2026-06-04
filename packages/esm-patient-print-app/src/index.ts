376229f0c1e7180d12ed929a31f9f8991ae214c6
ToNyRANDRIAMANANTSOA <tonyrandriamanantsoa.to@gmail.com>
2026-06-04 19:11:07 +0300

    (feat) orders-selective-printing: scaffold and init printing module

---
+++ packages/esm-patient-print-app/src/index.ts
@@ -0,0 +1,23 @@
+import { getAsyncLifecycle, getSyncLifecycle, defineConfigSchema } from '@openmrs/esm-framework';
+import { configSchema } from './config-schema';
+import { moduleName } from './constants';
+
+const options = {
+  featureName: 'patient-print-app',
+  moduleName,
+};
+
+export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
+
+export function startupApp() {
+  defineConfigSchema(moduleName, configSchema);
+}
+
+// Root component
+export const root = getAsyncLifecycle(() => import('./root.component'), options);
+
+// Extensions
+
+// Modals
+
+// Workspaces
